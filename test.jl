578fd2325a4f5e43c6e23764005c94200d076612
DinhThanhPhongDo <50570186+DinhThanhPhongDo@users.noreply.github.com>
2021-10-27 23:55:05 +0200

    ergonomic changes

--- test.jl
+++ test.jl
@@ -8,19 +8,25 @@ a = dict["weight"]
 N = dict["N"][end]
 b = dict["b"]
 
-@show dict
-@show N
+function SolverModel(c,a,N,b)
 
-model = Model(GLPK.Optimizer)
-@variable(model, x[1:N], Int)
-@show sum(c[i]*x[i] for i in 1:N)
+    model = Model(GLPK.Optimizer)
+    @variable(model, x[1:N], Int)
+    @show sum(c[i]*x[i] for i in 1:N)
 
-@objective(model, Max, sum(c[i]*x[i] for i in 1:N))
+    @objective(model, Max, sum(c[i]*x[i] for i in 1:N))
 
-@constraint(model, sum(a[i]*x[i] for i in 1:N)<= b)
-@constraint(model, con[i=1:N],x[i] >= 0)
+    @constraint(model, sum(a[i]*x[i] for i in 1:N)<= b)
+    @constraint(model, con[i=1:N],x[i] >= 0)
 
-print(model)
-optimize!(model)
+    print(model)
+    optimize!(model)
 
-@show solution_summary(model, verbose=true)
+    #@show solution_summary(model, verbose=true)
+    return model
+
+    
+function greedy(c,a,N,b)
+
+    x = { i:0 for i in 1:N}
+    return x
