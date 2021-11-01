133f1d1f8d2e9dab020f70b152af69acff0d0290
DinhThanhPhongDo <50570186+DinhThanhPhongDo@users.noreply.github.com>
2021-11-01 10:52:21 +0100

    commit

--- test.jl
+++ test.jl
@@ -1,17 +1,16 @@
-import GLPK,JuMP, JSON, Base
-using GLPK,JuMP, JSON, Base
+import GLPK,JuMP, JSON, Base, Gurobi
+using GLPK,JuMP, JSON, Base, Gurobi
 
 
-dict = JSON.parsefile("large1.json")
+dict = JSON.parsefile("medium.json")
 c = dict["utility"]
 a = dict["weight"]
 N = dict["N"][end]
 b = dict["b"]
-#print( c ./ a)
 
 function SolverModel(c,a,N,b)
     print("==============================Gurobi/ GLPK================================")
-    model = Model(Gurobi.Optimizer)
+    model = Model(GLPK.Optimizer)
     @variable(model, x[1:N], Int)
     @show sum(c[i]*x[i] for i in 1:N)
 
@@ -55,8 +54,9 @@ function greedy(c,a,N,b)
         end
     
     end
-
+    
     objective_value = sum(c[i]*x[i] for i in 1:N)
+    x = sort(collect(x), by=x->x[2])
     return objective_value, current_weight,x
 end
 
@@ -86,48 +86,110 @@ function DynamicProgramming(c,a,N,b)
     end
     return table[N+1,b+1]
 end
+
 function DynamicProgramming2(c,a,N,b)
     println("==============================DynamicProgramming==============================")
     b = convert(UInt16,b)
     table = zeros(Int64, N+1, b+1) 
-    dict = Dict(i => (c[i],a[i]) for i in 1:N)
+    dict = Dict(i => (c[i],a[i]) for i in 1:N)#c=profit,a=poids
     for i in 1:N+1
-
+        
         for w in 1:b+1
+            if (i == 1 || w == 1) #initialisation
+                table[i,w] = 0
+            else
+                tmax=b/dict[i-1][2]
+                tmax=floor(tmax, digits=0)
+                tmax=convert(UInt16,tmax)
+                
+                for t in 0:tmax
 
-            # t to modify according to textbook p42 and not from 0 to 1000
-            for t in 0:1000
-
-                #Im not too sure how to create properly flag, but this one is to avoid to cycle too much in the "t loop"
-                flag =false
-
-                if (i == 1 || w == 1) #initialisation
-                    table[i,w] = 0
-
-                #tant que le poids n'est pas suffisant
-                elseif t*dict[i-1][2] <= w-1
-
-                    flag =true
-                    #dict[i-1] acces the current item. table[i-1] acces the previous item! Sorry but Julia index begin. 
-                    #Si tu trouves une façon de rendre les indexs plus lisible, tu peux toujours modifier
-                    table[i,w] = max(table[i-1, w],
-                                    t *dict[i-1][1] + table[i-1, w - t*dict[i-1][2]],
-                                    table[i,w])
-                end
-
-                if flag == false
-                    break
-                end
+                    #Im not too sure how to create properly flag, but this one is to avoid to cycle too much in the "t loop"
+                    flag =false
+                    #tant que le poids n'est pas suffisant
+                    if t*dict[i-1][2] <= w-1
+                        
+                        flag =true
+                        #dict[i-1] acces the current item. table[i-1] acces the previous item! Sorry but Julia index begin. 
+                        #Si tu trouves une façon de rendre les indexs plus lisible, tu peux toujours modifier
+                        table[i,w] = max(table[i-1, w],
+                                        t *dict[i-1][1] + table[i-1, w - t*dict[i-1][2]],
+                                        table[i,w])
+                    end
+    
+                    if flag == false
+                        break
+                    end
+                end                
             end
         end
     end
-    return table[N+1,b+1]
+
+    #get x
+    x = Dict(i => 0 for i in 1:N)
+    w = b+1
+    for i in 1:N+1
+        #println(i-1)
+        #println(table[i,:])
+    end
+
+    for i in (N+1:-1:2)
+            #println(i-1)
+    
+        
+        if table[i,w] == table[i-1,w]
+            
+            x[i-1] = 0
+            #println((i-1,x[i-1]))
+            continue
+        else
+            tmax=(w-1)/dict[i-1][2]
+            tmax=floor(tmax, digits=0)
+            tmax=convert(UInt16,tmax)  
+            #print("tmax =")    
+            #println(tmax)
+            #print("actual weight= ")
+            #println(w)
+            #print("first")
+            #println(i-1)
+
+            flag = false
+            for t in (tmax:-1:1)
+                println(t)
+                if (table[i,w]- table[i-1, w - t*dict[i-1][2]]) % dict[i-1][1] == 0  
+                    flag = true
+                    w = w-t*dict[i-1][2]
+                    #print("weight=")
+                    #println(w)
+                    x[i-1] = t
+                    #println((i-1,x[i-1]))
+                    break
+
+                end
+            end
+
+            #if flag == false
+            #    println("there is a problem")
+            #    println(i-1)
+            #end
+            
+        end
+    end
+
+
+
+    #print("my sum is:")
+    #println(sum(c[i]*x[i] for i in 1:N))
+     x = sort(collect(x), by=x->x[2], rev = true)
+    return table[N+1,b+1],x
 end
+
+
 
 model = SolverModel(c,a,N,b)
 @show objective_value(model)
 
-println(greedy(c,a,N,b)[1])
+println(greedy(c,a,N,b))
 
 #for the medium problem, we can obtain the same results as the GLPK solver. Very slow for large problem. 
 #there exist also a DynamicProgramming solver, but this one is the one from the github and is only for the 0-1 knapsack problem i think
