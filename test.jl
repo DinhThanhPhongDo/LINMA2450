ff82726f27fc662d7250ec3ea62ef813b34aeb0c
DinhThanhPhongDo <50570186+DinhThanhPhongDo@users.noreply.github.com>
2021-11-02 22:03:49 +0100

    commit

--- test.jl
+++ test.jl
@@ -10,8 +10,8 @@ b = dict["b"]
 
 function SolverModel(c,a,N,b)
     print("==============================Gurobi/ GLPK================================")
-    model = Model(Gurobi.Optimizer)
-    set_optimizer_attribute(model, "Presolve", 0)
+    model = Model(GLPK.Optimizer)
+   # set_optimizer_attribute(model, "Presolve", 0)
    # set_optimizer_attribute(model, "heuristic", 0)
    # set_optimizer_attribute(model, "cut generation", 0)
     @variable(model, x[1:N], Int)
@@ -127,33 +127,9 @@ function DynamicProgramming2(c,a,N,b)
             end
         end
     end
-
-            # t to modify according to textbook p42 and not from 0 to 1000
-            for t in 0:1000#dict[i][2]/b
-
-                #Im not too sure how to create properly flag, but this one is to avoid to cycle too much in the "t loop"
-                flag =false
-
-                if (i == 1 || w == 1) #initialisation
-                    table[i,w] = 0
-
-                #tant que le poids n'est pas suffisant
-                elseif t*dict[i-1][2] <= w-1
-                    println(b/dict[i][2])
-                    flag =true
-                    #dict[i-1] acces the current item. table[i-1] acces the previous item! Sorry but Julia index begin. 
-                    #Si tu trouves une façon de rendre les indexs plus lisible, tu peux toujours modifier
-                    table[i,w] = max(table[i-1, w],
-                                    t *dict[i-1][1] + table[i-1, w - t*dict[i-1][2]],
-                                    table[i,w])
-                end
     #get x
     x = Dict(i => 0 for i in 1:N)
     w = b+1
-    for i in 1:N+1
-        #println(i-1)
-        #println(table[i,:])
-    end
 
     for i in (N+1:-1:2)
             #println(i-1)
