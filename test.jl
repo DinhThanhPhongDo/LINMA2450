1174ea6270beeb296b4fd741dfc832ac11cac089
DinhThanhPhongDo <50570186+DinhThanhPhongDo@users.noreply.github.com>
2021-11-03 13:47:41 +0100

    optimize the code

--- test.jl
+++ test.jl
@@ -1,8 +1,8 @@
-import GLPK,JuMP, JSON, Base, Gurobi
-using GLPK,JuMP, JSON, Base, Gurobi
+import GLPK,JuMP, JSON, Base, Gurobi, ProgressBars
+using GLPK,JuMP, JSON, Base, Gurobi, ProgressBars
 
 
-dict = JSON.parsefile("small.json")
+dict = JSON.parsefile("large1.json")
 c = dict["utility"]
 a = dict["weight"]
 N = dict["N"][end]
@@ -11,11 +11,10 @@ b = dict["b"]
 function SolverModel(c,a,N,b)
     print("==============================Gurobi/ GLPK================================")
     model = Model(Gurobi.Optimizer)
-    set_optimizer_attribute(model, "Presolve", 0)
-    set_optimizer_attribute(model, "Heuristics", 0)
-    set_optimizer_attribute(model, "Cuts", 0)
+    #set_optimizer_attribute(model, "Presolve", 0)
+    #set_optimizer_attribute(model, "Heuristics", 0)
+    #set_optimizer_attribute(model, "Cuts", 0)
     @variable(model, x[1:N], Int)
-    @show sum(c[i]*x[i] for i in 1:N)
 
     @objective(model, Max, sum(c[i]*x[i] for i in 1:N))
 
@@ -24,7 +23,7 @@ function SolverModel(c,a,N,b)
 
     optimize!(model)
 
-    @show solution_summary(model, verbose=true)
+    @show solution_summary(model,verbose=true)
     return model
 end
 
@@ -35,57 +34,42 @@ function greedy(c,a,N,b)
     utility = c ./ a #division terme par terme
 
     list     = [ (i, utility[i]) for i in 1:N]
-    dict     = Dict( i=> c[i]/a[i] for i in 1:N)
-    Base.sort!(list, by = x -> x[2], rev = true) #silent function applied on list
+
+    Base.sort!(list, by = x -> x[2], rev = true) 
 
     x = Dict(i => 0 for i in 1:N)
     
-    previous_weight = -1.0
-    current_weight = 0.0
-
-    while (current_weight != previous_weight)
-        previous_weight = current_weight
-
-        for (i,u) in list
-
-            while current_weight+a[i] <b
-                x[i] += 1.0
-                current_weight += a[i]
-            end
+    current_weight = 0
+    for (i,u) in list
+        if current_weight + a[i]< b
+            x[i] = floor((b-current_weight)/a[i],digits = 0)
+            current_weight += x[i]* a[i]
         end
-    
     end
     
     objective_value = sum(c[i]*x[i] for i in 1:N)
     x = sort(collect(x), by=x->x[2], rev = true)
     return objective_value, current_weight,x
 end
-function DynamicProgramming2(c,a,N,b)
+function DynamicProgramming(c,a,N,b)
     println("==============================DynamicProgramming==============================")
 
     b = convert(UInt16,b)
     table = zeros(Int64, N+1, b+1) 
     dict = Dict(i => (c[i],a[i]) for i in 1:N)
-    for i in 1:N+1
+    for i in ProgressBar(1:N+1)
+
         for w in 1:b+1
             if (i == 1 || w == 1) #initialisation
                 table[i,w] = 0
             else
-                tmax=b/dict[i-1][2]
-                tmax=floor(tmax, digits=0)
+                tmax=floor(w/dict[i-1][2], digits=0)
                 tmax=convert(UInt16,tmax)
-                for t in 0:tmax
-                    flag =false
-                    if t*dict[i-1][2] <= w-1
-                        flag =true
-                        table[i,w] = max(table[i-1, w],
-                                        t *dict[i-1][1] + table[i-1, w - t*dict[i-1][2]],
-                                        table[i,w])
-                    end
-                    if flag == false
-                        break
-                    end
-                end                
+                #print("utility is :")
+                #println( tmax)
+                
+                totest = [ t *dict[i-1][1] + table[i-1, w - t*dict[i-1][2]] for t in 0:tmax if t*dict[i-1][2] <= w-1]
+                table[i,w] = findmax(totest)[1]
             end
         end
     end
@@ -117,9 +101,13 @@ function DynamicProgramming2(c,a,N,b)
 end
 
 @time model = SolverModel(c,a,N,b)
-@show objective_value(model)
+
 
 @time aa,bb,cc=greedy(c,a,N,b)
 println(aa,cc)
+println(bb)
 
-@time println(DynamicProgramming2(c,a,N,b))
+@time v,x = DynamicProgramming(c,a,N,b)
+
+println(v)
+println(x)
